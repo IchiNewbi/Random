834331e74dafe1f834a6d898e68aba8a44a280f5
TeamChespin <stelladraco27@gmail.com>
2017-11-04 14:41:51 -0700

    Update connect4.cpp

--- Project_Connect4/connect4.cpp
+++ Project_Connect4/connect4.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-#define EMPTY_CELL_VALUE		'_'
-#define MAXIMUM_ROWS			(6)
-#define MAXIMUM_COLS			(7)
+#define EMPTY_CELL_VALUE		' '//what the empty cell values are set to
+#define MAXIMUM_ROWS			(6)//maximum rows
+#define MAXIMUM_COLS			(7)//maximum columns
 
 void setup(char board[7][6])//board is set to [7][6] which represents column and row
 {
@@ -21,15 +21,15 @@ void setup(char board[7][6])//board is set to [7][6] which represents column and
 void draw_board(char board[7][6])//the board is drawn from the board array
 {
 	cout << "====================="<<endl;
-	for (int row = 0; row < 6; row++)
+	for (int row = 5; row >= 0; row--)//draws each row
 	{
-		for (int col = 0; col < 7; col++)
+		for (int col = 0; col < 7; col++)//draws each column peace in the row
 		{
-			cout <<"|"<<board[col][row]<<"|";//draws each character in each row
+			cout <<"|"<<board[col][row]<<"|";//draws column peice
 		}
-		cout <<endl;//breaks the line each row down
+		cout <<endl;//breaks the line for each loop
 	}
-	cout << "=====================" << endl;
+	cout << "=====================" << endl;//ends with a line break
 }
 
 char getBoardValue(char board[7][6], int row, int col)
@@ -56,21 +56,42 @@ int main()
 {
 
 	char board[7][6];
-	setup(board);
+	setup(board);//generates and fills empty spaces in the board
 
-	int p = 1;
-	int c;
-	int game_win = 0;
+	int p = 1;//player that starts first
+	int game_win = 0;//default is zero so the game loop runs
 
-	while (game_win == 0)
+	while (game_win == 0)//runs as long as nobody has won
 	{
-		system("cls");
-		draw_board(board);
-		cout << endl;
-		cout << "What column do you, player " << p << ", want to place your piece?" << endl;//IMPORTANT STUFF DO THIS PLZ!!!!!!!!!!!!!!!!!!!!!
-		cin >> c;
-		drop(c, p);
-		p = 1 - p;
+
+		bool v = 0;//presets the error return for the answer validation seen below to false
+		do
+		{
+			int c;//presets the choice variable (c=choice)
+			system("cls");//clears the game screen for game rendering
+			if (bool == 1)
+			{
+				cout << "That column does not exist" << endl;
+			}
+			else
+			{
+
+			}
+			draw_board(board);//draws previous board for their reference
+			cout << endl;//break
+			cout << "What column do you, player " << p << ", want to place your piece?" << endl;//text for the choice
+			cin >> c;//recieves users choice
+			if (c > 7)//tests if answer is a valid column number
+			{//bad
+				v = 1;
+			}
+			else
+			{//good
+				v = 0;
+			}
+		} while (v == 1);//repeat if error
+		drop(c, p);//drop in column from player
+		p = 1 - p;//flips the player variable
 	}
 
     return 0;
