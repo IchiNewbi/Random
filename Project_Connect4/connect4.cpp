ac8980dab5c5701aeb14e9da705dbf0508a94ed6
TeamChespin <stelladraco27@gmail.com>
2017-11-13 21:01:20 -0800

    Update connect4.cpp

--- Project_Connect4/connect4.cpp
+++ Project_Connect4/connect4.cpp
@@ -1,48 +1,69 @@
+// ConsoleApplication5.cpp : Defines the entry point for the console application.
+//
 
 #include "stdafx.h"
+
 #include <iostream>
 
 using namespace std;
 
-#define EMPTY_CELL_VALUE		' '//what the empty cell values are set to
-#define MAXIMUM_ROWS			(6)//maximum rows
-#define MAXIMUM_COLS			(7)//maximum columns
+#define EMPTY_CELL_VALUE		' '//what the empty cell values are set to (default = ' ')
+#define RED_CELL_VALUE          'x'//what the "red" teams disk looks like (default = 1)
+#define BLUE_CELL_VALUE         'o'//what the "blue" teams disk looks like (default = 2)
+#define MAXIMUM_ROWS			(6)//maximum rows (default = 6)
+#define MAXIMUM_COLS			(7)//maximum columns (default = 7)
 
-void setup(char board[7][6])//board is set to [7][6] which represents column and row
+void setup(int board[7][6])//board is set to [7][6] which represents column and row
 {
-	for (int row = 0; row < MAXIMUM_ROWS ; row++)//row is set to 0 and will repeat until row is greaterthan or equal to 6
+	for (int row = 0; row < MAXIMUM_ROWS; row++)//row is set to 0 and will repeat until row is greaterthan or equal to 6
 	{
-		for (int col = 0; col < MAXIMUM_COLS ; col++) {//col is set to 0 and will repeat until col is greaterthan or equal to 7
-			board[col][row] = EMPTY_CELL_VALUE ;//blank board spaces will be printed as this
+		for (int col = 0; col < MAXIMUM_COLS; col++) {//col is set to 0 and will repeat until col is greaterthan or equal to 7
+			board[col][row] = 0;//blank board spaces will be printed as this
 		}
 	}
 }
 
-void draw_board(char board[7][6])//the board is drawn from the board array
+char get_disk(int col, int row, int board[MAXIMUM_COLS][MAXIMUM_ROWS]) {
+	if (board[col][row] == 0) {
+		return EMPTY_CELL_VALUE;
+	}
+	else if (board[col][row] == 1) {
+		return RED_CELL_VALUE;
+	}
+	else if (board[col][row] == 2) {
+		return BLUE_CELL_VALUE;
+	}
+	else {
+		return 'v';
+	}
+}
+
+void draw_board(int board[MAXIMUM_COLS][MAXIMUM_ROWS])//the board is drawn from the board array
 {
-	cout << "====================="<<endl;
-	for (int row = 5; row >= 0; row--)//draws each row
+	cout << "=====================" << endl;
+	for (int row = MAXIMUM_ROWS - 1; row >= 0; row--)//draws each row
 	{
-		for (int col = 0; col < 7; col++)//draws each column peace in the row
+		for (int col = 0; col < MAXIMUM_COLS; col++)//draws each column peace in the row
 		{
-			cout <<"|"<<board[col][row]<<"|";//draws column peice
+			char disk = get_disk(col, row, board);
+			cout << "|" << disk << "|";//draws column peice
 		}
-		cout <<endl;//breaks the line for each loop
+		cout << endl;//breaks the line for each loop
 	}
 	cout << "=====================" << endl;//ends with a line break
 }
 
-char getBoardValue(char board[7][6], int row, int col)
+char getBoardValue(int board[MAXIMUM_COLS][MAXIMUM_ROWS], int row, int col)
 {
 	return board[col][row];
 }
 
-bool isEmpty(char board[7][6], int row, int col)
+bool isEmpty(int board[MAXIMUM_COLS][MAXIMUM_ROWS], int row, int col)
 {
 	return getBoardValue(board, row, col) == EMPTY_CELL_VALUE;
 }
 
-bool isOccupied(char board[7][6], int row, int col)
+bool isOccupied(int board[MAXIMUM_COLS][MAXIMUM_ROWS], int row, int col)
 {
 	return !isEmpty(board, row, col);
 }
@@ -55,9 +76,9 @@ void drop(int c, int p)
 int main()
 {
 
-	char board[7][6];
+	int board[MAXIMUM_COLS][MAXIMUM_ROWS];
 	setup(board);//generates and fills empty spaces in the board
-
+	int c = 0;
 	int p = 1;//player that starts first
 	int game_win = 0;//default is zero so the game loop runs
 
@@ -69,7 +90,7 @@ int main()
 		{
 			int c;//presets the choice variable (c=choice)
 			system("cls");//clears the game screen for game rendering
-			if (bool == 1)
+			if (v == 1)
 			{
 				cout << "That column does not exist" << endl;
 			}
@@ -94,6 +115,5 @@ int main()
 		p = 1 - p;//flips the player variable
 	}
 
-    return 0;
+	return 0;
 }
-
